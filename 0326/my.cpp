ff8c7a8818c5fa66406aa3bd15d174e9274366b8
2sseul <iseul0626@gmail.com>
2025-03-26 15:27:07 +0900

    03.26 수업

---
+++ 0326/my.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+class Saram
+{
+	int iX;  //창자
+	int iY;  //간
+	int iAge;
+
+public:
+	void Assign(int a, int b)
+	{
+		if (a < 0 || b < 0)
+			return;
+
+		iX = a;
+		iY = b;
+		iAge = 23;
+	}
+	int Add() //심장
+	{
+		Assign(2, 3);
+		return iX + iY;
+	}
+};
+
+Saram gildong, youngja, cheolsu;
+
+void main()
+{
+	gildong.Assign(3, 5);
+
+	int iResult = gildong.Add();
+	
+	printf("두 개의 값을 더한 결과: %d\n", iResult);
+}
