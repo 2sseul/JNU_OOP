b41b36b00708888a59df311814e8e25240c3fef3
2sseul <iseul0626@gmail.com>
2025-05-07 14:48:52 +0900

    05.07 수업

---
+++ 0507/05.07.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string>
+
+class CBase {
+    public:
+        virtual std::string kkk() = 0; // 순수 가상 함수
+    
+        void WhoAreYou() {
+            std::string tmp = kkk();
+            printf("%s", tmp.c_str());
+            printf("--------\n");
+            printf("레전드교수님\n");
+            printf("실습이 경쟁력이다\n");
+            printf("오케이!\n");
+        }
+    };
+    
+    class CDrived : public CBase {
+    public:
+        void InitComponent() {
+            
+        }
+    };
+
+    CDrived xxx;
+
+    CBase* AfxGetApp(){
+        return &xxx;
+    }
+
+void main(){
+    CBase* p = AfxGetApp();
+    (*p).WhoAreYou();
+}
